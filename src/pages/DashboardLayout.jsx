964ed504c38e5569033f358416b8f5b727e0995d
JohnDoeee <yandex949@gmail.com>
2022-12-07 01:52:02 +0300

    edited layout

--- src/pages/DashboardLayout.jsx
+++ src/pages/DashboardLayout.jsx
@@ -1,19 +1,19 @@
 import React from "react"
-import { useSelector } from "react-redux"
-import { Navigate, Outlet } from "react-router-dom"
+// import { useSelector } from "react-redux"
+// import { Navigate, Outlet } from "react-router-dom"
 import { Header } from "../components/Header"
 import { Sidebar } from "../components/Sidebar"
-import { selectSidebar } from "../redux/features/sidebar/sidebarSlice"
-import { selectUser } from "../redux/features/user/userSlice"
+// import { selectSidebar } from "../redux/features/sidebar/sidebarSlice"
+// import { selectUser } from "../redux/features/user/userSlice"
 import "./styles/dashboard.scss"
 
 export const DashboardLayout = () => {
-	const { user } = useSelector(selectUser)
-	const { isOpen } = useSelector(selectSidebar)
+	// const { user } = useSelector(selectUser)
+	// const { isOpen } = useSelector(selectSidebar)
 
-	if (!("email" in user)) {
-		return <Navigate to="/landing" />
-	}
+	// if (!("email" in user)) {
+	// 	return <Navigate to="/landing" />
+	// }
 
 	return (
 		<div className={`layout ${isOpen ? "open" : ""}`}>
