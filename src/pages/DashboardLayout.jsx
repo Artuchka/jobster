4ca405e83ad38093aa58b2925b612a040ca1aede
JohnDoeee <yandex949@gmail.com>
2022-12-07 02:13:00 +0300

    axios + layout

--- src/pages/DashboardLayout.jsx
+++ src/pages/DashboardLayout.jsx
@@ -1,27 +1,27 @@
 import React from "react"
-// import { useSelector } from "react-redux"
-// import { Navigate, Outlet } from "react-router-dom"
+import { useSelector } from "react-redux"
+import { Navigate, Outlet } from "react-router-dom"
 // import { Header } from "../components/Header"
 // import { Sidebar } from "../components/Sidebar"
-// import { selectSidebar } from "../redux/features/sidebar/sidebarSlice"
-// import { selectUser } from "../redux/features/user/userSlice"
+import { selectSidebar } from "../redux/features/sidebar/sidebarSlice"
+import { selectUser } from "../redux/features/user/userSlice"
 import "./styles/dashboard.scss"
 
 export const DashboardLayout = () => {
-	// const { user } = useSelector(selectUser)
-	// const { isOpen } = useSelector(selectSidebar)
-	// if (!("email" in user)) {
-	// 	return <Navigate to="/landing" />
-	// }
-	// return (
-	// 	<div className={`layout ${true ? "open" : ""}`}>
-	// 		<div className="sidebar">{/* <Sidebar /> */}</div>
-	// 		<div className="sidebar__bg"></div>
-	// 		<div className="main">
-	// 			{/* <Header /> */}
-	// 			{/* <Outlet /> */}
-	// 		</div>
-	// 	</div>
-	// )
-	return <div>ahah</div>
+	const { user } = useSelector(selectUser)
+	const { isOpen } = useSelector(selectSidebar)
+	if (!("email" in user)) {
+		return <Navigate to="/landing" />
+	}
+	return (
+		<div className={`layout ${true ? "open" : ""}`}>
+			<div className="sidebar">{/* <Sidebar /> */}</div>
+			<div className="sidebar__bg"></div>
+			<div className="main">
+				{/* <Header /> */}
+				{/* <Outlet /> */}
+			</div>
+		</div>
+	)
+	// return <div>ahah</div>
 }
