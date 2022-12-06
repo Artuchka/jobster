6c9544e87d1895363280aa85ac835d7c0b4cbb24
JohnDoeee <yandex949@gmail.com>
2022-12-07 01:46:27 +0300

    toast + route wrapper

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-// import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { ToastContainer } from "react-toastify"
 // import { AddJob } from "./pages/AddJob.jsx"
 // import { AllJobs } from "./pages/AllJobs"
