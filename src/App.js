ff5b4507cac4e801a7fef2bfd9bc0e691a65fa0e
JohnDoeee <yandex949@gmail.com>
2022-12-07 01:49:10 +0300

    +home +layout

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { ToastContainer } from "react-toastify"
 // import { AddJob } from "./pages/AddJob.jsx"
 // import { AllJobs } from "./pages/AllJobs"
-// import { DashboardLayout } from "./pages/DashboardLayout"
+import { DashboardLayout } from "./pages/DashboardLayout"
 import { Error } from "./pages/Error"
-// import { Home } from "./pages/Home"
+import { Home } from "./pages/Home"
 import { Landing } from "./pages/Landing"
 // import { Profile } from "./pages/Profile"
 import { Register } from "./pages/Register"
@@ -29,12 +29,12 @@ function App() {
 
 			<BrowserRouter>
 				<Routes>
-					{/*<Route path="/" element={<DashboardLayout />}>
+					<Route path="/" element={<DashboardLayout />}>
 						<Route index element={<Home />} />
-						<Route path="all-jobs" element={<AllJobs />} />
+						{/*<Route path="all-jobs" element={<AllJobs />} />
 						<Route path="add-job" element={<AddJob />} />
-						<Route path="profile" element={<Profile />} />
-					</Route>*/}
+						<Route path="profile" element={<Profile />} />*/}
+					</Route>
 					<Route path="/landing" element={<Landing />} />
 					<Route path="/register" element={<Register />} />
 					<Route path="*" element={<Error />} />
